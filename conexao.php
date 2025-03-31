ae3a702be7641bc4cfbedda605c5737d75c6bf98
Guilherme Lins <guilherme.santos@modalgr.io>
2025-03-31 16:20:18 -0300

    feat: definir inserção de aluno

--- conexao.php
+++ conexao.php
@@ -3,3 +3,5 @@
 $caminhoBanco = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $caminhoBanco);
 echo 'Conectado.';
+
+$pdo->exec('CREATE TABLE students(id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT)');
