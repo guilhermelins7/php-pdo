a5154d56ab29a93dfe718f04052f07636183b41b
Guilherme Lins <guilherme.santos@modalgr.io>
2025-04-01 16:03:38 -0300

    feat: implementar busca por id

---
+++ lista-aluno-por-id.php
@@ -0,0 +1,24 @@
+<?php
+
+use Alura\Pdo\Domain\Model\Student;
+
+require_once 'vendor/autoload.php';
+
+// ID DE BUSCA:
+$id = 1;
+
+$caminhoBanco = __DIR__ . '/banco.sqlite';
+$pdo = new PDO('sqlite:' . $caminhoBanco);
+
+
+$result = $pdo->query( "SELECT * FROM students WHERE id = $id;");
+
+if ($studentData = $result->fetch(PDO::FETCH_ASSOC)) {
+    $student = new Student(
+        $studentData['id'],
+        $studentData['name'],
+        new DateTimeImmutable($studentData['birth_date'])
+    );
+
+    echo $student->age() . PHP_EOL;
+}
