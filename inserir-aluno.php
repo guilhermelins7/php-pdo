050bfd15cd21b38db0896f820ecc36adca3e9e23
Guilherme Lins <guilherme.santos@modalgr.io>
2025-04-03 10:54:44 -0300

    refactor: melhorar clareza do projeto

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -4,14 +4,15 @@ use Alura\Pdo\Domain\Model\Student;
 
 require_once 'vendor/autoload.php';
 
+// Chamando diretório atual:
+$caminhoBanco = __DIR__ . '/banco.sqlite';
+$pdo = new PDO('sqlite:' . $caminhoBanco);
+
 echo "Adicionar aluno:\nInforme o nome: ";
 $nomeAluno = trim(fgets(STDIN));
 
 echo "Informe DT de nascimento: 'Y-m-d':";
 $dataNascimento = trim(fgets(STDIN));
-
-$caminhoBanco = __DIR__ . '/banco.sqlite';
-$pdo = new PDO('sqlite:' . $caminhoBanco);
 
 $student = new Student(
     null,
@@ -19,10 +20,12 @@ $student = new Student(
     new DateTimeImmutable($dataNascimento)
 );
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (? , ?);";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name , :birth_date);";
+// Preparando a query para execução:
 $statement = $pdo->prepare($sqlInsert);
-$statement->bindValue(1, $student->name());
-$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
+// Associando o placeholder com os dados do Objeto:
+$statement->bindValue(':name', $student->name());
+$statement->bindValue(':birth_date', $student->birthDate()->format('Y-m-d'));
 
 if($statement->execute()) {
     echo 'Aluno incluido!';
