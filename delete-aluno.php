9e80f27f289a7d37c8a519fa17691ee5dcc5b998
Guilherme Lins <guilherme.santos@modalgr.io>
2025-04-03 11:01:46 -0300

    refactor: implementar prepare statment no delete-aluno

--- delete-aluno.php
+++ delete-aluno.php
@@ -4,14 +4,17 @@ use Alura\Pdo\Domain\Model\Student;
 
 require_once 'vendor/autoload.php';
 
-echo "Deletar aluno\nInforme o ID:";
-$idDelete = trim(fgets(STDIN));
-
 $caminhoBanco = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $caminhoBanco);
 
-$result = $pdo->exec( "DELETE FROM students WHERE id = '$idDelete';");
+echo "Deletar aluno\nInforme o ID:";
+$idDelete = trim(fgets(STDIN));
 
+$statment = $pdo->prepare("DELETE FROM students WHERE id = :id");
+// Definindo o bindValue como inteiro.
+$statment->bindValue(':id', $idDelete, PDO::PARAM_INT);
+
+$result = $statment->execute();
 if ($result) {
     echo $result . " excluido."; exit();
 }
