4ff9a29fd6de7cf929e47ce9c2f6cb6e0e684ac2
Guilherme Lins <guilherme.santos@modalgr.io>
2025-04-08 16:06:46 -0300

    feat: definir criação tabela telefones

--- conexao.php
+++ conexao.php
@@ -2,8 +2,26 @@
 
 use Alura\Pdo\Infrastructure\Persistence\ConnectionBD;
 
+require_once __DIR__ . '/vendor/autoload.php';
+
 $pdo = ConnectionBD::createConnection();
 
-$pdo->exec('CREATE TABLE students(id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT)');
+$createTablesSql = '
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        birth_date TEXT
+    );
+
+    CREATE TABLE IF NOT EXISTS phones (
+        id INTEGER PRIMARY KEY,
+        area_code TEXT,
+        number TEXT,
+        student_id INTEGER,
+        FOREIGN KEY (student_id) REFERENCES students(id)
+    );
+';
+
+$pdo->exec($createTablesSql);
 
 echo 'Conectado.';
