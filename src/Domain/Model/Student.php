2842dfa23aef4e554b4ffbbd56ad2d8656ff234c
Guilherme Lins <guilherme.santos@modalgr.io>
2025-04-08 16:23:31 -0300

    feat: definir entidade Phone

--- src/Domain/Model/Student.php
+++ src/Domain/Model/Student.php
@@ -2,11 +2,15 @@
 
 namespace Alura\Pdo\Domain\Model;
 
+use Alura\Pdo\Domain\Model\Phone
+
 class Student
 {
     private ?int $id;
     private string $name;
     private \DateTimeInterface $birthDate;
+    /** @var Phone[] */
+    private array $phones;
 
     public function __construct(?int $id, string $name, \DateTimeInterface $birthDate)
     {
@@ -50,4 +54,18 @@ class Student
             ->diff(new \DateTimeImmutable())
             ->y;
     }
+
+    public function addPhone(Phone $phone): void
+    {
+        $this->phones[] = $phone;
+    }
+
+    /**
+     * @return Phone[]
+     */
+    public function phones(): array
+    {
+        return $this->phones;
+    }
+
 }
